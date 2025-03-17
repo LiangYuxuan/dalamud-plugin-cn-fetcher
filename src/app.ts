6a4b3addd3048d05d51e72edb0c8385562ad8f75
LiangYuxuan <lyx@lyx.es>
2025-03-17 17:13:06 +0800

    feat: patch 7.2 global date

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,10 @@ interface VersionInfo {
 // https://ff.web.sdo.com/web8/index.html#/newstab/newslist
 const versions: VersionInfo[] = [
     {
+        version: '7.2',
+        global: new Date('2025-03-25T08:00:00Z'),
+    },
+    {
         version: '7.1',
         global: new Date('2024-11-12T08:00:00Z'),
         cn: new Date('2025-02-18T08:00:00Z'),
